e54073a98e9d9ea9a6ac6750317b268ce7dc0793
tarskyie <tafunum@outlook.com>
2026-02-14 11:38:18 +0900

    4th commit

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+python manage.py auto_createsuperuser --username ivantarsky --email tarsky2@example.com --password securepassword
